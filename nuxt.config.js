b1760f731f939503b32f48ff296402586f4275df
endijulian <endijulian080798@gmail.com>
2022-09-08 17:48:33 +0700

    landing page, integrasi backend

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
     link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap' }],
   },
 
-  loading: { color: "#2085dd" },
+  loading: { color: "#2085dd", },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
@@ -44,7 +44,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'http://localhost:8080',
   },
 
   auth: {
--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -159,23 +159,6 @@
         </div>
       </section>
       <div class="cta-clip -mt-20"></div>
-      <section class="call-to-action bg-purple-progress pt-64 pb-10">
-        <div class="container mx-auto">
-          <div class="w-full text-center">
-            <h1 class="text-5xl text-white font-semibold">
-              Easy way to funding
-              <br />
-              best idea and innovation
-            </h1>
-            <a
-              href="/register.html"
-              class="inline-block bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 mt-8 text-lg rounded-full"
-            >
-              Getting Start
-            </a>
-          </div>
-        </div>
-      </section>
 
       <CallToAction/>
 
